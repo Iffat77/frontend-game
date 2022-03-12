1efe69380fd5605238d0bff34c2179b0573d68c6
Iffat Hossain <iffathossain@iffats-air.mynetworksettings.com>
2022-03-12 18:59:13 -0500

    created form for input of word choice

---
+++ script.js
@@ -0,0 +1,23 @@
+//Have a player enter a word that will be guessed during the game.
+// The word is then hidden and represented by blank spaces.
+//The second player then chooses letters,
+// which are revealed if present.
+
+
+
+//start
+//<------->
+// create input for player one and hide it
+// display alphabet on screen or create letter bank
+//begin game logic
+//<-------->
+//player2 guesses letters for word
+//if the letters match the input letter is revealed
+//if letters do not match add a point
+//while this happens if player2 score reaches 6 they loose 
+//if all letters of input word are revealed before player2 reaches 6 they win.
+
+
+//players have an option to input the whole word
+
+
