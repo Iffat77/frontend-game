e498c383891a37c80081076fa5d67e55d73c866e
Iffat Hossain <iffathossain@Iffats-MacBook-Air.local>
2022-03-15 12:31:48 -0400

    created a letterbank on screen and added eventlistener for click events

--- script.js
+++ script.js
@@ -25,22 +25,67 @@ let formBtn = document.querySelector("#form")
 let inputText = document.querySelector(".inputText")
 let wordArray = []
 let wordTxt = document.querySelector("#wordTxt")
+let letters = document.querySelectorAll(".letter")
 
-// listener for form submission 
+// listener for submit event
+// taking word from input and returning it as an array of srtings stored in the variable wordArray 
 formBtn.addEventListener("submit", (e) => {
   e.preventDefault()
   const data = Object.fromEntries(new FormData(e.target).entries());
   let { word } = data
+  console.log(word)
   wordArray = word.toLowerCase().split("")
-  console.log(wordArray)
-
-  
   formBtn.reset()
+
 })
 
-// .filer
+//listening for click event on letters 
+// 
+
+letters.forEach(letter => letter.addEventListener("click", (e) => {
+   e.preventDefault() 
+  if (wordArray.includes(letter.innerHTML) === true) {
+    console.log("good guess")
+  } else if (wordArray.includes(letter.innerHTML) === false) {
+ console.log("get a job!")
+  }
+  
+  // console.log(wordArray.includes(letter.innerHTML ))
+}))
+
+
+
+
+
+
+
+// console.log(letters[3].innerText)
+
+// compare letters in wordArray to letter clicked 
+
+// function compareLetter() {
+//   if(DataTransfer.letter ===)
+// }
+
+
+
+
+
+// .filer543
 // find index of each 
 
 
-
+// create blank space with underline to hold wordArray
+// nested arrays to hold keyboard choices 
+// event listener & query all on keyboard to listen for letter choice 
+// - conditional 
+// if selected letter exists in wordaArray then display visible 
+// selected letters are no longer clickable on keyboard 
+// -- clicked keys can be stored in a new variable 
+// else if add a strike to player score 
+// create while looop stating 
+// after each input - while player score is less than 6 
+// continue 
+//else player looses 
+//
 
